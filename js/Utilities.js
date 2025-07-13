cf352924b5b4f69f481820368ed60376c8d6d645
Christina Hollie <christinahollie7@gmail.com>
2025-07-13 00:15:17 +0000

    syled draggable  box and message box

--- js/Utilities.js
+++ js/Utilities.js
@@ -1,3 +1,14 @@
 function showCoordinates(x, y) {
-  alert(`New coordinates: X=${x}, Y=${y}`);
+  const box = document.getElementById('message-box');
+  box.textContent = `📍 Your new position: X = ${x}, Y = ${y}`;
+  box.classList.remove('hidden');
+
+  // Auto-hide after 3 seconds
+  setTimeout(() => {
+    box.classList.add('hidden');
+  }, 3000);
 }
+
+// function showCoordinates(x, y) {
+//   alert(`Way to go! You moved the box to these new coordinates: X=${x}, Y=${y}`);
+// }
